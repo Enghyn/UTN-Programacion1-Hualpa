37356417c9eeace39cb80e516cdb2501fed3bbbb
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-09-11 21:36:57 -0300

    mejora a ejercicio patentes

--- ejercicios_clase_4_9/ejercicio_patentes.py
+++ ejercicios_clase_4_9/ejercicio_patentes.py
@@ -7,45 +7,56 @@ while True:
 
 while True:
     numero_a_ingresar = int(input("Ingrese número a sumar a la patente: "))
-    if 0 <= numero_a_ingresar <= 17576000:
+    if 0 <= numero_a_ingresar < 17576000:
         break
     else:
         print("Número inválido o fuera del rango posible. Intente nuevamente.")
 
 ABECEDARIO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-NUMERO_MAXIMO = 9
-LONGITUD_PATENTE = 6
+NUMERO_MAXIMO = 999
 
 patente_numero = []
 for elemento in PATENTE:
     if elemento.isalpha():
         patente_numero.append(ABECEDARIO.index(elemento))
     else:
-        patente_numero.append(int(elemento))
+        patente_numero.append(int(PATENTE[3:]))
+        break
 
+if numero_a_ingresar > 676000:
+    patente_numero[0] += numero_a_ingresar // 676000
+    numero_a_ingresar = numero_a_ingresar % 676000
+    if numero_a_ingresar > 26000:
+        patente_numero[1] += numero_a_ingresar // 26000
+        numero_a_ingresar = numero_a_ingresar % 26000
+        if numero_a_ingresar > 1000:
+            patente_numero[2] += numero_a_ingresar // 1000
+            numero_a_ingresar = numero_a_ingresar % 1000
+            if numero_a_ingresar > 0:
+                patente_numero[3] += numero_a_ingresar
 
-fuera_de_rango = False
-for i in range(numero_a_ingresar):
-    patente_numero[5] += 1
-    for i in range(LONGITUD_PATENTE - 1, -1, -1):
-        if i >= 3:
-            if patente_numero[i] > NUMERO_MAXIMO:
-                patente_numero[i] -= NUMERO_MAXIMO + 1
-                patente_numero[i - 1] += 1
-        elif 3 > i > 0:
-            if patente_numero[i] > len(ABECEDARIO):
-                patente_numero[i] -= len(ABECEDARIO)
-                patente_numero[i - 1] += 1
-        else:
-            if patente_numero[i] > len(ABECEDARIO):
-                print("Patente fuera de rango")
-                fuera_de_rango = True
+LONGITUD_PATENTE = len(patente_numero)
 
-if not fuera_de_rango:
-    nueva_patente = ""
-    for i in range(LONGITUD_PATENTE):
-        if i <= 2:
-            nueva_patente += ABECEDARIO[patente_numero[i]]
+for i in range(LONGITUD_PATENTE - 1, -1, -1):
+    if i == 3 and patente_numero[i] > NUMERO_MAXIMO:
+        patente_numero[i] -= NUMERO_MAXIMO + 1
+        patente_numero[i - 1] += 1
+    elif 3 > i > 0 and patente_numero[i] >= len(ABECEDARIO):
+        patente_numero[i] -= len(ABECEDARIO)
+        patente_numero[i - 1] += 1
+    elif i == 0 and patente_numero[i] >= len(ABECEDARIO):
+        print("Patente fuera de rango")
+        exit()
+
+nueva_patente = ""
+for i in range(LONGITUD_PATENTE):
+    if i < 3:
+        nueva_patente += ABECEDARIO[patente_numero[i]]
+    else:
+        if patente_numero[i] < 10:
+            nueva_patente += "00" + str(patente_numero[i])
+        elif 10 <= patente_numero[i] < 100:
+            nueva_patente += "0" + str(patente_numero[i])
         else:
             nueva_patente += str(patente_numero[i])
-    print(f"Nueva patente: {nueva_patente}")
+print(f"Nueva patente: {nueva_patente}")
